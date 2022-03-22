c1bb1aa4df19cfd0f29ce3b65ea1aa7398b96916
Salmonpro <58506154+Salmonpro@users.noreply.github.com>
2022-03-22 11:32:08 +0100

    line

--- racetest.ino
+++ racetest.ino
@@ -153,3 +153,17 @@ void loop() {
     ledcWrite(6, value);
     delay(500);
   }
+  int LineTracking(){ 
+  int sensorL = analogRead (leftSensor);
+  int sensorR = analogRead (rightSensor);
+  int bustSpeed = 255;    // MAX 255 
+  int boost_delay = 3000;
+  
+  if(sensorR > 300 && sensorL > 300)
+  {
+    Serial.println("boooooost");
+  
+    turnRight(bustSpeed);
+    delay(boost_delay);
+  
+  }
