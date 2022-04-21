8a768f334fecec1aa05b333cf7b06f6ad7a164a8
Salmonpro <58506154+Salmonpro@users.noreply.github.com>
2022-04-21 12:55:56 +0200

    Update race_2_monday.ino

--- race_2_monday/race_2_monday.ino
+++ race_2_monday/race_2_monday.ino
@@ -1,64 +1,116 @@
- #include <Wire.h>
+#include "Adafruit_VL53L0X.h"
+#include <Arduino.h>
+#include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <hcsr04.h>
 #include <ESP32Servo.h>
+#include <hcsr04.h>
 
-#define TRIG_PIN 14
-#define ECHO_PIN 12
+#define TRIG_PIN 25
+#define ECHO_PIN 26
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
-@ -11,20 +16,25 @@
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
-HCSR04 hcsr04(TRIG_PIN, ECHO_PIN, 20, 4000);
+HCSR04 left_dist_sensor(TRIG_PIN, ECHO_PIN, 20, 4000);
 
+const byte led_gpio = 17; // the PWM pin the LED is attached to
+const byte led_gpio1 = 5; // the PWM pin the LED is attached to
+
+const byte back1 = 16; // the PWM pin the LED is attached to
+const byte back2 = 18; // the PWM pin the LED is attached to
+
+
+long duration;
+float distanceCm;
+
+bool checked = false;
 
 // the setup routine runs once when you press reset:
 void setup() {
   Serial.begin(115200);
 
- void setup() {
+  ledcAttachPin(led_gpio, 0); // assign a led pins to a channel
+  ledcAttachPin(led_gpio1, 1); // assign a led pins to a channel
+  ledcAttachPin(back1, 2); // assign a led pins to a channel
+  ledcAttachPin(back2, 3); // assign a led pins to a channel
+
+  // Initialize channels
+  // channels 0-15, resolution 1-16 bits, freq limits depend on resolution
+  // ledcSetup(uint8_t channel, uint32_t freq, uint8_t resolution_bits);
+  ledcSetup(0, 4000, 8); // 12 kHz PWM, 8-bit resolution
+  ledcSetup(1, 4000, 8); // 12 kHz PWM, 8-bit resolution
   ledcSetup(2, 4000, 8); // 12 kHz PWM, 8-bit resolution
   ledcSetup(3, 4000, 8); // 12 kHz PWM, 8-bit resolution
 
-  Serial.begin(115200);
+
 
   // wait until serial port opens for native USB devices
   while (! Serial) {
     delay(1);
   }
-  
-  Serial.println("Adafruit VL53L0X test");
   //------Distance Sensor Test----------
-  if (!lox.begin()) {
+
+    if (!lox.begin()) {
     Serial.println(F("Failed to boot VL53L0X"));
     while (1);
-  }
-  // power 
-  Serial.println(F("VL53L0X API Simple Ranging example\n\n"));
+    }
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  VL53L0X_RangingMeasurementData_t measure;
-  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
 
 
-  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
-  //displayDistance();
+
+  //LineTracking()
+
+    
+    if (getDistanceF() > 200) {
+        goForward(180);
+    } else {
+        stopCar();
+        if (getDistanceR() > getDistanceL()) {
+        while (getDistanceF() < 200 && getDistanceL() > 10) { // rotation algorithm
+            turnRight(190);
+            delay(50);
+        }
+        } else {
+        while (getDistanceF() < 200 && getDistanceR() > 10) { // rotation algorithm
+            turnLeft(190);
+            delay(50);
+        }
+        }
+    }
+
 
   }
-
-}
 
 
 int goForward(int speed) {
   ledcWrite(0, speed);
   ledcWrite(1, speed);
-@ -108,6 +142,83 @@ int turnRight(int speed){
+  ledcWrite(2, 0);
+  ledcWrite(3, 0);
+}
+
+int goBack(int speed) {
+  ledcWrite(0, 0);
+  ledcWrite(1, 0);
+  ledcWrite(2, speed);
+  ledcWrite(3, speed);
+}
+
+int turnLeft(int speed) {
+  ledcWrite(0, speed);
+  ledcWrite(1, 0);
+  ledcWrite(2, 0);
+  ledcWrite(3, speed);
+}
+
+int turnRight(int speed) {
+  ledcWrite(0, 0);
+  ledcWrite(1, speed);
+  ledcWrite(2, speed);
   ledcWrite(3, 0);
 }
 
@@ -85,6 +137,18 @@ int lookForward() {
   delay(500);
 }
 
+int getDistanceF() {
+  return middle_dist_sensor.distanceInMillimeters();
+}
+
+int getDistanceR() {
+  return right_dist_sensor.distanceInMillimeters();
+}
+
+int getDistanceL() {
+  return left_dist_sensor.distanceInMillimeters();
+}
+
 int findWay() {
   VL53L0X_RangingMeasurementData_t measure;
   int distance = 0;
@@ -101,14 +165,10 @@ int findWay() {
     }
     delay(6);
   }
-  //turnRight(175);
-  //delay(600);
+
 
   stopCar();
-  //lox.rangingTest(&measure, false);
-  //Serial.println("on right distance is ");
-  //Serial.println(measure.RangeMilliMeter);
-  //distance = measure.RangeMilliMeter;
+ 
   delay(500);
   for (int i = 0; i < 20; i++) {
     turnLeft(175);
@@ -118,21 +178,15 @@ int findWay() {
     }
     delay(12);
   }
-  //turnLeft(175);
-  //delay(1200);
+
   stopCar();
   delay(500);
-  //lox.rangingTest(&measure, false);
-  //Serial.println("on left distance is ");
-  //Serial.println(measure.RangeMilliMeter);
+ 
   if (distance < distance1 )
   {
     return 1;
   } else {
-    /*turnRight(175);
-      delay(920);
-      goForward(0); // stop
-      break; */
+
     return 0;
   }
 
@@ -142,4 +196,15 @@ int findWay() {
 int displayDistance() {
   VL53L0X_RangingMeasurementData_t measure;
 
+
+  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  // Display static text
+  display.println(measure.RangeMilliMeter);
+  display.display();
 }
